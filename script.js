d885ee82bc5c6de73beed0aa4fa7ff6230a8c575
Eric Abell <ericabell@users.noreply.github.com>
2017-08-08 13:25:40 -0500

    Input fed to search by appending to url
    Results come back to console.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let url = 'http://recipepuppyproxy.herokuapp.com/api/?q=omelet';
+// let url = 'http://recipepuppyproxy.herokuapp.com/api/?q=omelet';
+let url = 'http://recipepuppyproxy.herokuapp.com/api/?q=';
 
 let divForSearchResults = document.querySelector('.search-results');
 let inputSearchTerm = document.querySelector('#search-term');
@@ -7,13 +8,18 @@ let searchButton = document.querySelector('#search-button');
 searchButton.addEventListener('click', (event) => {
   // grab the contents of the search field
   let searchString = inputSearchTerm.value;
-  console.log(searchString);
+  console.log(`User searched for: ${searchString}`);
+
+  let newSearchURL = url + searchString;
+  searchForRecipes(newSearchURL);
 })
 
-fetch(url)
-  .then( (response) => {
-    return response.json();
-  })
-  .then( (data) => {
-    console.log(data);
-  })
+function searchForRecipes(url) {
+  fetch(url)
+    .then( (response) => {
+      return response.json();
+    })
+    .then( (data) => {
+      console.log(data);
+    })
+}
