8242452831aea7e6db6b2b3d9e65a2fc386c4e16
Eric Abell <ericabell@users.noreply.github.com>
2017-08-08 13:23:06 -0500

    Get basic input from user works

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let url = 'http://recipepuppyproxy.herokuapp.com/api/?q=omelet';
 
 let divForSearchResults = document.querySelector('.search-results');
+let inputSearchTerm = document.querySelector('#search-term');
+let searchButton = document.querySelector('#search-button');
+
+searchButton.addEventListener('click', (event) => {
+  // grab the contents of the search field
+  let searchString = inputSearchTerm.value;
+  console.log(searchString);
+})
 
 fetch(url)
   .then( (response) => {
